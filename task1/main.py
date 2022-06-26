e8394ab7860fb0f4d5a9388195a6e9cc847ccd69
Alireza Saei <saeialireza359@gmail.com>
2022-06-26 17:22:05 +0430

    Added necessary functions

--- task1/main.py
+++ task1/main.py
@@ -9,5 +9,41 @@ camera = data['camera']
 cars = data['cars']
 
 
+def printMatrix(matrix):
+    for row in matrix:
+        print(*row)
+
+
+def getDimension(n):
+    row = size[0] - ((n-1) // size[1]) - 1
+    column = (n - 1) % size[1]
+
+    return row, column
+
+
+def buildMatrix():
+    matrix = [[0 for _ in range(size[1])] for _ in range(size[0])]
+    ic, jc = getDimension(camera)
+    matrix[ic][jc] = "C"
+
+    count = 1
+    for car in cars:
+        i1, j1 = getDimension(car[0])
+        i2, j2 = getDimension(car[1])
+        matrix[i1][j1] = count
+        matrix[i2][j2] = count
+        count += 1
+    return matrix
+
+
 def solve():
-    pass
+    if matrix[it][jt] == 0:
+        return True
+
+    for c in cars:
+        pass
+
+
+matrix = buildMatrix()
+printMatrix(matrix)
+it, jt = getDimension(target)
