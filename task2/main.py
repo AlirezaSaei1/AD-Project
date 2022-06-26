5cf2c016e6b17ee4390774ea91ff7cdd69aa12ed
AliSK <aebrahimi6739@gmail.com>
2022-06-27 02:38:18 +0430

    edit menu and fix bugs

--- task2/main.py
+++ task2/main.py
@@ -3,11 +3,6 @@ from collections import deque
 INF = float('inf')
 
 
-def printMatrix(matrix):
-    for row in matrix:
-        print(*row)
-
-
 def o(c):
     return ord(c) - ord('A')
 
@@ -77,12 +72,12 @@ def print_road(places, src, dst):
 
 
 def TSP(W, n, curr, visited, path, cost, rem, answer):
-    if rem == 0:
+    if rem == 0 and 0 < W[curr][src] < INF:
         answer.append((cost + W[curr][src], path + [src]))
         return
 
     for next in range(n):
-        if not visited[next]:
+        if not visited[next] and 0 < W[curr][next] < INF:
             visited[next] = True
             path.append(next)
             TSP(W, n, next, visited, path, cost + W[curr][next], rem-1, answer)
@@ -132,37 +127,35 @@ def min_spanning_tree(W, n):
     return length, nearest
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    with open("task2/places.json", "r") as f:
+    with open('task2/places.json', 'r') as f:
         places = json.loads(f.read())
 
     n = len(places)
 
     roads = [[INF for _ in range(n)] for _ in range(n)]
 
-    with open("task2/roads.txt") as f:
+    with open('task2/roads.txt') as f:
         for road in f.readlines():
             src, dst, dist = road.strip().split()
             s, d, l = o(src), o(dst), int(dist)
             roads[s][s] = roads[d][d] = 0
             roads[s][d] = roads[d][s] = l
     while True:
-        print("Main Menu\n1. Task 2-1\n2. Task 2-2\n3. task 3\n4. Exit")
+        print('Main Menu\n1. Task 2-1\n2. Task 2-2\n3. Task 3\n4. Exit')
         inp = int(input())
         if inp == 1:
-            source = input("Source: ")
-            destination = input("Destination: ")
-            src = find_place(places, source)
-            dst = find_place(places, destination)
+            src = find_place(places, 'Parking')
+            dst = find_place(places, input('Enter destination: '))
             # src = find_place(places, 'Parking')
             # dst = find_place(places, 'University')
 
-            # task 2-1
             if dst == -1:
                 print('Invalid place name')
 
             else:
+                # task 2-1
                 length, parent = dijkstra(roads, n, src)
                 paths = find_paths(parent, dst, src)
                 print('Shortest path lenght: ', length[dst])
@@ -170,33 +163,37 @@ if __name__ == "__main__":
                     print_path(places, path)
         elif inp == 2:
             # task 2-2
-            source = input("Source: ")
-            src = find_place(places, source)
             #src = find_place(places, 'Hospital')
-            pathway = []
-            while True:
-                visitLocation = input("Visit Place (0: Exit): ")
-                if visitLocation == "0":
-                    break
-                else:
-                    pathway.append(visitLocation)
             #pathway = ['Park', 'Terminal', 'Restaurant']
+            src = find_place(places, input('Source:'))
+            print('Enter visit places in one line:')
+            pathway = [find_place(places, place) for place in input().split()]
             path = [src]
+
+            if -1 in path + pathway:
+                print('Invalid place name')
+                continue
+
             visited = [True] * n
-            for way in pathway:
-                visited[find_place(places, way)] = False
+            for place in pathway:
+                visited[place] = False
 
             answer = []
             TSP(roads, n, src, visited, path, 0, len(pathway), answer)
+
+            if len(answer) == 0:
+                print('No path exist')
+                continue
+
             cost, path = min(answer)
 
+            print('Min Length: ', cost)
             for c, p in answer:
                 if c == cost:
-                    print('Min Length: ', c)
                     print_path(places, p)
 
             # cost = TSP2(roads, n, src, visited, path, 0)
-            # print("Min Length: ", cost)
+            # print('Min Length: ', cost)
             # print_path(places, path)
         elif inp == 3:
             # task 3
