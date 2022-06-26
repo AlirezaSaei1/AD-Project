790748d0b0c0d17016c727d4644055ee2c1e0ab2
Alireza Saei <saeialireza359@gmail.com>
2022-06-27 00:34:03 +0430

    Backtracking function added

--- task1/main.py
+++ task1/main.py
@@ -42,14 +42,37 @@ def buildMatrix():
     return matrix
 
 
+def vertical(car):
+    return abs(car[1]-car[0]) != 1
+
+
 def solve(i, j):
     if promising(i, j):
+        printMatrix(matrix)
         if (i, j) == getDimension(target):
-            print('Solved')
+            # print_matrix(matrix)
+            print(answer)
+            print('solved')
             return
         else:
             for dir in moves:
-                pass
+                x, y = i+dir[0], j+dir[1]
+                if promising(x, y):
+                    car = cars[matrix[x][y]-1]
+                    x = i + len(car) * dir[0]
+                    y = j + len(car) * dir[1]
+                    if promising(x, y):
+                        if len(car) == 1 or\
+                                vertical(car) and dir in [(-1, 0), (1, 0)] or\
+                                not vertical(car) and dir in [(0, -1), (0, 1)]:
+                            history.append(matrixToString())
+                            print(x, y)
+                            matrix[i][j] = matrix[x][y]
+                            matrix[x][y] = 0
+                            solve(x, y)
+                            matrix[x][y] = matrix[i][j]
+                            matrix[i][j] = 0
+                            history.remove(matrixToString())
 
 
 def promising(i, j):
@@ -63,9 +86,13 @@ def promising(i, j):
 
 
 history = []
+answer = []
 moves = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+visited = [[False for _ in range(size[0])] for _ in range(size[1])]
 
 matrix = buildMatrix()
 printMatrix(matrix)
 it, jt = getDimension(target)
-solve(it, jt)
+
+# Indices of empty tile (15)
+solve(0, 2)
